946245183bb7bde2d838f3b0cffc9113b1d0e035
Mathumitha.M <mmathumitha@appen.com>
2024-05-22 23:17:38 +0530

    Entity and repository added

--- src/main/java/com/springsecurity/BankApplication/config/ProjectSecurityConfig.java
+++ src/main/java/com/springsecurity/BankApplication/config/ProjectSecurityConfig.java
@@ -1,4 +1,6 @@
 package com.springsecurity.BankApplication.config;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -21,14 +24,15 @@ import java.util.Set;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 public class ProjectSecurityConfig {
 
 
 
         @Bean
         public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-            http.authorizeHttpRequests((requests) -> requests
-                    .requestMatchers("/notices","/contacts").permitAll()
+            http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((requests) -> requests
+                    .requestMatchers("/notices","/contacts","/register/**").permitAll()
                     .anyRequest().authenticated()
             );
 
@@ -36,31 +40,12 @@ public class ProjectSecurityConfig {
             http.httpBasic(withDefaults());
             return http.build();
         }
-        /*
-       In Memory user detail Manager
-        @Bean
-        public InMemoryUserDetailsManager  userCreate(){
-            UserDetails admin = User.withDefaultPasswordEncoder()
-                                .username("admin")
-                                .password("123456")
-                                .authorities("admin").build();
-            UserDetails user = User.withDefaultPasswordEncoder()
-                    .username("user")
-                    .password("123456")
-                    .authorities("read").build();
-            Set<UserDetails> createUser = new HashSet<UserDetails>();
-            createUser.add(admin);
-            createUser.add(user);
-            return new InMemoryUserDetailsManager(createUser);
 
-
-
-        }*/
 
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
 
